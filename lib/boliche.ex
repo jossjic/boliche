42d31b791f64302a0168f994e6b894c24cf44f6a
José Juan <jossjic_03@hotmail.com>
2023-02-21 12:38:14 -0600

    Initial commit

---
+++ lib/boliche.ex
@@ -0,0 +1,18 @@
+defmodule Boliche do
+  @moduledoc """
+  Documentation for `Boliche`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Boliche.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/boliche_test.exs
@@ -0,0 +1,8 @@
+defmodule BolicheTest do
+  use ExUnit.Case
+  doctest Boliche
+
+  test "greets the world" do
+    assert Boliche.hello() == :world
+  end
+end
